4705cf717e05d4316c11d5568fd023095ae90737
JustinLung <justinlung77@gmail.com>
2022-07-14 22:56:43 +0200

    :construction: fetching data

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,14 @@
-<h1>Hello World</h1>
+<script>
+	import * as helper from '@prismicio/helpers';
+	export let page;
+</script>
+
+<main>
+	{JSON.stringify(page, null, 2)}
+	<!-- <section>
+		<h2>{page.data?.title}</h2>
+	</section>
+	{#each standards as standard, i}
+    <p>{standard.data.}</p>
+  {/each} -->
+</main>
