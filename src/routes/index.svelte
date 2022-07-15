0f1acb8d67375ca9be6d2832d855d0f20459d2aa
JustinLung <justinlung77@gmail.com>
2022-07-15 09:06:41 +0200

    :sparkles: data fetching

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,11 +2,13 @@
 	import * as helpers from '@prismicio/helpers';
 	export let page;
 	export let standards;
+	console.log(page);
 </script>
 
 <main>
 	<section>
-		<h2>{page.data?.title}</h2>
-		<p>{page.data?.introduction}</p>
+		<!-- <h2>{page.data?.title}</h2> -->
+		<h2>{page.data.title}</h2>
+		{@html helpers.asHTML(page.data?.introduction)}
 	</section>
 </main>
