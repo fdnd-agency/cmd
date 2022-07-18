db72024b48dc9bf4c614e9ac6646db9c70088f47
JustinLung <justinlung77@gmail.com>
2022-07-18 19:01:30 +0200

    page transitions made

---
+++ src/lib/components/PageTransition.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { fly } from 'svelte/transition';
+</script>
+
+<div in:fly={{ y: -5, duration: 500, delay: 500 }} out:fly={{ y: -5, duration: 500 }}>
+	<slot />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,23 +4,26 @@
 	import Seperator from '$lib/components/Seperator.svelte';
 	import StandardSection from '$lib/components/IndexComponents/StandardSection.svelte';
 	import StandardChapter from '$lib/components/IndexComponents/StandardChapter.svelte';
+	import PageTransition from '$lib/components/PageTransition.svelte';
 
 	export let page;
 	export let standards;
 </script>
 
-<main>
-	<Hero heroTitle={page.data.title} />
-	<Introduction introduction={page.data?.introduction} />
-	{#each standards as standard, i}
-		<StandardSection
-			standardUID={`/${standard?.uid}`}
-			standardTitle={`${i + 1}.0 ${standard.data?.title}`}
-		>
-			<Seperator />
-			{#each standard.data?.chapters as chapter, i}
-				<StandardChapter chapterTitle={`.${i + 1} ${chapter.data?.title}`} />
-			{/each}
-		</StandardSection>
-	{/each}
-</main>
+<PageTransition>
+	<main>
+		<Hero heroTitle={page.data.title} />
+		<Introduction introduction={page.data?.introduction} />
+		{#each standards as standard, i}
+			<StandardSection
+				standardUID={`/${standard?.uid}`}
+				standardTitle={`${i + 1}.0 ${standard.data?.title}`}
+			>
+				<Seperator />
+				{#each standard.data?.chapters as chapter, i}
+					<StandardChapter chapterTitle={`.${i + 1} ${chapter.data?.title}`} />
+				{/each}
+			</StandardSection>
+		{/each}
+	</main>
+</PageTransition>
