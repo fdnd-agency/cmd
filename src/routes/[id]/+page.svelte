ba3f85aaa68bd89d63e7f355297482c5f4886f43
MokhtarAkle <mokhtar.akle@hva.nl>
2024-02-12 10:19:15 +0100

    Added title element to each page

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -5,6 +5,10 @@
 	let workform = data.workform[0];
 </script>
 
+<svelte:head>
+	<title>{workform.title} · Platform Grote Thema's</title>
+</svelte:head>
+
 <main>
 	<section>
 		<div class="upload-button">
